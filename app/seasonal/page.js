62ae38b2a829ae6d2cb6bdcd4a4be3b5a7fc9c4f
Samarth Pai <samarthpai9870@gmail.com>
2025-08-22 14:53:58 +0530

    fix: added suspend in /seasonal

--- app/seasonal/page.js
+++ app/seasonal/page.js
@@ -1,9 +1,10 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
+import { Suspense } from 'react'
 
-const Seasonal = () => {
-    const params = useSearchParams()
+const Seasonal = ({ prms }) => {
+    const params = useSearchParams();
     const season = params.get("season");
     const year = params.get("year");
     const [animes, setAnimes] = useState([])
@@ -14,34 +15,42 @@ const Seasonal = () => {
         const getData = async () => {
             const seasonReq = await fetch(`https://api.jikan.moe/v4/seasons/${year}/${season}`)
             const seasonJson = await seasonReq.json()
-            console.log(seasonJson)
             setAnimes(seasonJson['data'])
         }
         getData()
     }, [])
 
     return (
-        <div>
-            <div className='bgimage'>
-                <img src={`${season}-bg.jpg`} className='w-[100%] h-[100%] object-cover object-center fixed' alt="" />
-            </div>
-            <div className='px-[10vw] pb-3 text-3xl pt-20 font-bold relative'>
-                {seasonName} {year}
-            </div>
-            {/* <div className={`absolute top-[${fadePosition[season]}px] left-0 h-32 w-full bg-gradient-to-t from-black/80 to-transparent pointer-events-none z-0`} /> */}
-            <div className='cards flex flex-wrap gap-3 z-0 relative mx-10 justify-center mb-10'>
-                {animes && animes.length && animes.map((item, ind) => {
-                    return <div key={ind} className="border border-white/50 shadow-black rounded-xl p-3 w-60 cursor-pointer bg-black/50 backdrop-blur-2xl" onClick={() => router.push(`anime/${item['mal_id']}`)}>
-                        <div className="h-85 rounded-xl overflow-hidden relative">
-                            <img src={item['images']['webp']['image_url']} className="w-60 h-full object-cover" />
-                            <span className="absolute bottom-0 right-1 text-xl font-extrabold text-shadow-md text-shadow-amber-950">{item['score']}</span>
+        <>
+            <div>
+                <div className='bgimage'>
+                    <img src={`${season}-bg.jpg`} className='w-[100%] h-[100%] object-cover object-center fixed' alt="" />
+                </div>
+                <div className='px-[10vw] pb-3 text-3xl pt-20 font-bold relative'>
+                    {seasonName} {year}
+                </div>
+                {/* <div className={`absolute top-[${fadePosition[season]}px] left-0 h-32 w-full bg-gradient-to-t from-black/80 to-transparent pointer-events-none z-0`} /> */}
+                <div className='cards flex flex-wrap gap-3 z-0 relative mx-10 justify-center mb-10'>
+                    {animes && animes.length && animes.map((item, ind) => {
+                        return <div key={ind} className="border border-white/50 shadow-black rounded-xl p-3 w-60 cursor-pointer bg-black/50 backdrop-blur-2xl" onClick={() => router.push(`anime/${item['mal_id']}`)}>
+                            <div className="h-85 rounded-xl overflow-hidden relative">
+                                <img src={item['images']['webp']['image_url']} className="w-60 h-full object-cover" />
+                                <span className="absolute bottom-0 right-1 text-xl font-extrabold text-shadow-md text-shadow-amber-950">{item['score']}</span>
+                            </div>
+                            <div>{item['title_english'] ? item['title_english'] : item['title']}</div>
                         </div>
-                        <div>{item['title_english'] ? item['title_english'] : item['title']}</div>
-                    </div>
-                })}
+                    })}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default Seasonal
+const SuspendedSeasonal = () => {
+    return (<Suspense>
+        <Seasonal />
+    </Suspense>
+    )
+}
+
+export default SuspendedSeasonal
